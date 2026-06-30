a25c43ccc4daa19bf451aefb8a0c54f9a4dcbb3f
Tunahan Kabak <tunahan.kabak@hotmail.com>
2026-06-30 10:44:56 +0300

    upd

--- apps/web_app/src/pages/index.tsx
+++ apps/web_app/src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head';
 import App from '../app';
 
 export default function Home() {
-  return <App initialTab="user" />;
+  return (
+    <>
+      <Head>
+        <title>Hoppa - Kuzey Kıbrıs'ın Yerel Teslimat ve Market Uygulaması</title>
+        <meta name="description" content="Hoppa ile KKTC'deki en sevdiğiniz restoranlardan yemek siparişi verin, market alışverişinizi dakikalar içinde kapınıza getirin." />
+      </Head>
+      <App initialTab="user" />
+    </>
+  );
 }
